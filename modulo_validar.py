a2c042bcca99b6aedb881fd0ea157b07af939d87
samux <samueleliasib28@gmail.com>
2016-05-13 17:20:18 -0430

    correccion

--- modulo_validar.py
+++ modulo_validar.py
@@ -1,6 +1,13 @@
-import validar, validar_password
+from validar import validar_usuario
+from validar_password import password
 
-validar.user()
+usuario=raw_input ('ingrese el usuario: ')
 
-validar_password.password ()
+clave=raw_input ('ingrese la clave: ')
+
+if validar_usuario(usuario) == True and password(clave) == True:
+    print 'datos correctos'
+
+else:
+    print 'datos incorrectos'
 
